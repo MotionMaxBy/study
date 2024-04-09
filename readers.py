213526f767613ead1d566fa66c99cd7f774196ac
MotionMaxBy <166320153+MotionMaxBy@users.noreply.github.com>
2024-04-09 17:07:36 +0300

    Class ReaderYAML update

--- readers.py
+++ readers.py
@@ -8,23 +8,28 @@ class ReaderYAML:
 
     def __init__(self, file_path):
         self.__file_path = file_path
-        self.__text = self.read_yaml()
+        self.__text = self.read()
 
-    def read_yaml(self):
+    def read(self):
         try:
             with open(self.__file_path, "r") as f:
                 res = yaml.load(f, Loader=yaml.Loader)
         except Exception as e:
             self.__errors = e
             return None
+        self.__errors = None
         return res
 
-    def write_yaml(self):
+    def write(self):
         try:
             with open(self.__file_path, "w") as f:
                 yaml.dump(self.__text, f)
         except Exception as e:
             self.__errors = e
+        self.__errors = None
+
+    def remove_to(self, file_path):
+        pass
 
     @property
     def text(self):
@@ -33,7 +38,15 @@ class ReaderYAML:
     @text.setter
     def text(self, text):
         self.__text = text
-        self.write_yaml()
+        self.write()
+
+    @property
+    def path(self):
+        return self.__file_path
+
+    @path.setter
+    def path(self, file_path):
+        self.__file_path = file_path
 
     @property
     def is_empty(self):

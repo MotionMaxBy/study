7baf21ec3d2adcc87d09b9f4d6a3cade7c16a42f
MotionMaxBy <166320153+MotionMaxBy@users.noreply.github.com>
2024-04-19 16:24:27 +0300

    Reformat to object

--- weather/weather.py
+++ weather/weather.py
@@ -1,38 +1,75 @@
 import requests
+import json
 import time
 from config import API_KEY
 
 
-def get_geodata(city):
-    URL = "http://api.openweathermap.org/geo/1.0/direct"
-    params = {
-        "q": city,
-        "limit": 1,
-        "appid": str(API_KEY)
-    }
-    return requests.get(URL, params=params)
+class GeoPosition:
+    name: str
+    lon: str
+    lat: str
+    local_names: list
+    main_info: dict
+    weather: list
+    status: int
+    response: str
+    geodata_url = "http://api.openweathermap.org/geo/1.0/direct"
+    weather_url = "https://api.openweathermap.org/data/2.5/weather"
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_geodata(self):
+        params = {
+            "q": self.name,
+            "limit": 1,
+            "appid": str(API_KEY)
+        }
+        api_response = requests.get(self.geodata_url, params=params)
+        if api_response.status_code == 200:
+            for place in api_response.json():
+                self.local_names = place['local_names']
+                self.lat = place['lat']
+                self.lon = place['lon']
+        self.status = api_response.status_code
+        self.response = api_response.text
+
+    def __repr__(self):
+        return f"{self.name} (lon: {self.lon} lat: {self.lat} )"
+
+    @property
+    def json(self):
+        return json.dumps({self.name: {"lon": self.lon, "lat": self.lat, "local_names": self.local_names}})
+
+    @json.setter
+    def json(self, json_dict):
+        for key, value in json_dict.values():
+            self.name = key
+            self.lat = value.get("lat")
+            self.lon = value.get("lon")
+            self.local_names = value.get("local_names")
+
+    def get_weather(self):
+        params = {
+            "lat": self.lat,
+            "lon": self.lon,
+            "appid": str(API_KEY),
+            "units": "metric",
+            "lang": "ru",
+        }
+        api_weather_response = requests.get(self.weather_url, params=params)
+        if api_weather_response.status_code == 200:
+            self.main_info = api_weather_response.json().get('main')
+            self.weather = api_weather_response.json().get('weather')
 
 
-def get_weather(place_lat, place_lon):
-    URL = "https://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "lat": place_lat,
-        "lon": place_lon,
-        "appid": str(API_KEY),
-        "units": "metric",
-        "lang": "ru",
-    }
-    return requests.get(URL, params=params)
+city = GeoPosition('Гомель')
+city.get_geodata()
 
-
-response = get_geodata('Гомель')
-if response.status_code == 200:
-    for city in response.json():
-        print(city['local_names']['ru'], city['lat'], ":", city['lon'], end=', ')
-
-        city_weather = get_weather(city["lat"], city["lon"])
-        print("Температура:", city_weather.json().get('main').get('temp'), end=', ')
-        print(city_weather.json().get('weather')[0].get('description'))
+if city.status == 200:
+    city.get_weather()
+    print(city.name, "температура:", city.main_info.get('temp'), end=', ')
+    print(city.weather[0].get('description'))
 else:
-    print("Requests error (weather):", response.status_code)
-    print(response.text)
+    print("Requests error (weather):", city.status)
+    print(city.response)
